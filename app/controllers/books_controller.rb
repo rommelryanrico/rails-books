b676fe6649b25600c27629e45022633b1e206007
rommelryanrico <rommelryanrico@hotmail.com>
2019-04-15 23:00:08 +1000

    add background image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,29 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.all()
   end
+
+  def new
+    @books = Book.new
+  end
+
+  def create
+    @book =
+    Book.new(book_params)
+      if @book.save
+        redirect_to '/books'
+      else
+        render 'new'
+      end
+    end
 
   def show
     @book = Book.find(params[:id])
   end
 end
+
+private
+  	  def book_params
+      
+    params.require(:book).permit(:title)
+      end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'books#index'
+  get '/home' => 'books#home'
+  get '/books' => 'books#index'
+  get '/books/new' => 'books#new'
+  post '/books' => 'books#create'
   get '/books/:id', to: 'books#show'
+  get '/books/:id/edit' => 'books#edit'
+  put '/books/:id' => 'books#update'
+  delete '/books/:id' => 'books#destroy'
+
+  root to: 'books#home'
 end
