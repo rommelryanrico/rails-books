51d2cea02b0211b02210d861745c5d33a5156dea
rommelryanrico <rommelryanrico@hotmail.com>
2019-04-15 12:19:52 +1000

    book model setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Start of Seeding..."
+Book.destroy_all
+50.times do
+  params = {
+    title: Faker::Book.unique.title,
+    author: Faker::Book.unique.author,
+    publisher: Faker::Book.publisher,
+    genre: Faker::Book.genre,
+    description: Faker::Lorem.paragraph  
+  }
+  puts "Creating Book: #{params}"
+  book = Book.new(params)
+  book.save
+end
+
+puts "Seeding Over"
