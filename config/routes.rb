38a1db4d85208e74d9e02ed4dc4101273c3b51ad
rommelryanrico <rommelryanrico@hotmail.com>
2019-04-17 02:05:20 +1000

    Complete Book App

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/books", to: "books#index", as: "books"
   get "/books/new", to: "books#new", as: "new_book"
   post '/books', to: 'books#create'
-  get "/books/:id", to: "books#show", as: "show_book"
+  get "/books/:id", to: "books#show", as: "book"
   get "/books/:id/edit", to: "books#edit", as: "edit_book"
   patch '/books/:id', to: 'books#update'
   put '/books/:id', to: 'books#update'
